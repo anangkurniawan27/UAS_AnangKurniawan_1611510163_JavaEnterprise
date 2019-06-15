6d0e8c0028a8486c795dcea7e624157615f8e752
Anang Kurniawan <161510163@student.budiluhur.ac.id>
2019-06-16 01:18:21 +0700

    UAS_AnangKurniawan_1611510163_JavaEnterprise

---
+++ librarydb.sql
@@ -0,0 +1,32 @@
+﻿# Host: localhost  (Version 5.5.5-10.1.16-MariaDB)
+# Date: 2019-06-15 22:49:09
+# Generator: MySQL-Front 5.4  (Build 3.46)
+# Internet: http://www.mysqlfront.de/
+
+/*!40101 SET NAMES utf8 */;
+
+#
+# Structure for table "books"
+#
+
+DROP TABLE IF EXISTS `books`;
+CREATE TABLE `books` (
+  `id` int(11) NOT NULL,
+  `name_book` varchar(30) NOT NULL,
+  `id_kategori` int(11) NOT NULL,
+  `desk` varchar(30) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+#
+# Structure for table "categories"
+#
+
+DROP TABLE IF EXISTS `categories`;
+CREATE TABLE `categories` (
+  `id` int(11) NOT NULL,
+  `name_book` varchar(30) NOT NULL,
+  `code_book` varchar(30) NOT NULL,
+  `desk` varchar(30) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
